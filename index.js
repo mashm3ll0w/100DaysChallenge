9b738e0d390be86c20691447371b96e819b88cf8
Charles Swaleh <charlswall@live.com>
2020-08-22 23:20:27 +0300

    recap day01 - fetch and XMLHttpsRequest

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // 01 - Convert JSON data to HTML
 document.addEventListener("DOMContentLoaded", () => {
 	document.getElementById("getContent").onclick = () => {
@@ -44,3 +45,28 @@ document.getElementById("sendMsg").onclick = () => {
 	let body = JSON.stringify({ userName: userName, suffix: " loves cats" });
 	xhr.send(body);
 };
+=======
+document.addEventListener("DOMContentLoaded", () => {
+	document.getElementById("getBtn").onclick = () => {
+		let req = new XMLHttpRequest();
+		req.open("GET", "https://swapi.dev/api/people/4/", true);
+		req.responseType = "json";
+		req.send();
+
+		req.onload = function () {
+			let data = req.response;
+			console.log(data);
+			document.querySelector(".heading1").innerText = data.name;
+			document.querySelector(".tagline1").innerText = `Gender: ${data.gender} Eye Color: ${data.eye_color}`;
+		};
+
+		fetch("https://www.breakingbadapi.com/api/characters/1/")
+			.then((response) => response.json())
+			.then((data) => {
+        console.log(data);
+				document.querySelector(".heading2").innerText = data[0].name;
+				document.querySelector(".tagline2").innerText = `Occupation: ${data[0].occupation.join(", ")}`;
+			});
+	};
+});
+>>>>>>> 309933a... recap day01 - fetch and XMLHttpsRequest
