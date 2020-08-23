b34b430d560951645bfc98c264961095b492ed0f
Charles Swaleh <charlswall@live.com>
2020-08-24 00:22:49 +0300

    day03 - post data using XMLHttpRequest

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 // 01 - Convert JSON data to HTML
 document.addEventListener("DOMContentLoaded", () => {
 	document.getElementById("getContent").onclick = () => {
@@ -70,3 +71,35 @@ document.addEventListener("DOMContentLoaded", () => {
 	};
 });
 >>>>>>> 309933a... recap day01 - fetch and XMLHttpsRequest
+=======
+// 01 - DOMContentLoaded
+// document.addEventListener("DOMContentLoaded", function () {
+// 	document.getElementById("getMessage").onclick = function () {
+// 		document.getElementsByClassName("message")[0].innerText = "Here is the message";
+// 	};
+// });
+
+// 02 - XMLRequests - Declare the json object beforehand
+// document.addEventListener("DOMContentLoaded", function () {
+// 	document.getElementById("getMessage").onclick = () => {
+// 		let req = new XMLHttpRequest();
+// 		req.open("GET", "https://swapi.dev/api/people/1/", true);
+// 		req.responseType = "json";
+// 		req.send();
+// 		req.onload = () => {
+// 			let ans = req.response;
+// 			document.querySelector(".message").innerText = ans.name;
+// 		};
+// 	};
+// });
+
+// 02a - Declare the json object afterwards
+// let req = new XMLHttpRequest();
+// req.open("GET", "https://swapi.dev/api/people/1/", true);
+// req.send();
+
+// req.onload = () => {
+// 	let json = JSON.parse(req.response);
+// 	document.querySelector(".message").innerText = JSON.stringify(json.eye_color);
+// };
+>>>>>>> ab027be... DOMContentLoaded & XMLHttpRequest
