091e2307dc5a9996936a2d5c2bdce8cea9605143
Charles Swaleh <charlswall@live.com>
2020-08-27 21:37:06 +0300

    day0 - async functions day02

--- index.js
+++ index.js
@@ -5,17 +5,36 @@
 // example 01
 downloadPhoto("https://download-from-site.com/photo.jpeg", handlePhoto);
 
-function handlePhoto(error, photo){
-  if (error){
-    console.error("Download error!", error);
-  }
-  else{
-    console.log("Download finished", photo)
-  }
+function handlePhoto(error, photo) {
+	if (error) {
+		console.error("Download error!", error);
+	} else {
+		console.log("Download finished", photo);
+	}
 }
 
 console.log("Download started...");
 
 // downloadPhoto function is called and handlePhoto is passed as its callback...
 // the callback wont run until the downloadPhoto runs and either returns an error or success...
-// 
+//
+
+// example 02
+// read a file and do something with the contents
+
+const fs = require("fs");
+var myNumber = "";
+
+function addOne(callback) {
+	fs.readFile("myFile.txt", function doneReading(err, fileContents) {
+		myNumber = parseInt(fileContents);
+		myNumber++;
+		callback();
+	});
+}
+
+function logNumber() {
+	console.log(myNumber);
+}
+
+addOne(logNumber);
