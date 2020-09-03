a2e754f48e7c4e24c9111e72b5f30b1e68dbbd42
Charles Swaleh <charlswall@live.com>
2020-09-03 21:09:42 +0300

    day12: FCC challenge - find and replace

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
-document.querySelector("body").style.textAlign = "center";
-document.querySelector(".tag").style.fontSize = "1.75rem";
-document.querySelector(".tag").style.color = "indigo";
-
-// challenge 01 - Seek and destroy
-// function destroyer(arr) {
-// 	let baseArr = arguments[0];
-// 	let duplicates = [];
-// 	for (let x = 1; x < arguments.length; x++) {
-// 		duplicates.push(arguments[x]);
-// 	}
-// 	return baseArr.filter((item) => {
-// 		return duplicates.indexOf(item) === -1;
-// 	});
-// }
-
-// console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
-
-// challenge 01 - solution 2
-function destroyer(arr){
-  let data = arguments[0];
-  let items = [];
-  for(let x = 1; x < arguments.length; x++){
-    items.push(arguments[x]);
-  }
-  return data.filter(val => {
-    return !items.includes(val);
-  });
+function myReplace(str, before, after) {
+	return str.replace(before, after);
 }
 
-console.log(destroyer([1, 2, 3, 1, 2, 3, 4], 1, 2, 3));
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
