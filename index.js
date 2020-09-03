17dc5742ed99fff8138ab5e3a7929a28b4f79d3c
Charles Swaleh <charlswall@live.com>
2020-09-03 21:43:02 +0300

    day12: challenge 02 - find and replace

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function myReplace(str, before, after) {
-	return str.replace(before, after);
+	let capsReg = /[A-Z]/;
+	if (before[0] === before[0].toUpperCase()) {
+		let newAfter = [...after];
+		newAfter[0] = newAfter[0].toUpperCase();
+		return str.replace(before, newAfter.join(""));
+	} else {
+		return str.replace(before, after.toLowerCase());
+	}
 }
 
-myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+console.log(myReplace("Let us go to the store", "store", "mall"));
