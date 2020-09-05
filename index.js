d1133172d82a95976bd7274d980d776cfde1c85a
Charles Swaleh <charlswall@live.com>
2020-09-05 21:35:56 +0300

    day13: solve FCC challenge 02

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 // spinal tap
 // Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
 
-function spinalCase(str){
-	let myReg = /[" "_]/gi;
-	return str.replace(myReg, "-").toLowerCase();
+function spinalCase(str) {
+	// Create a variable for the white space and underscores.
+	let myRegex = /\s+|_+/g;
+
+	// Replace low-upper case to low-space-uppercase
+	str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+	
+	// Replace space and underscore with -
+	return str.replace(myRegex, "-").toLowerCase();
 }
 
 console.log(spinalCase("This Is Spinal Tap"));
+console.log(spinalCase("thisIsSpinalTap"));
+console.log(spinalCase("The_Andy_Griffith_Show"));
+console.log(spinalCase("Teletubbies say Eh-oh"));
+console.log(spinalCase("AllThe-small Things"));
